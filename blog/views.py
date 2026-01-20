8ee17d1b0d6efc93b99d106b121de9403e83188f
unknown <marciodev8@gmail.com>
2026-01-20 09:54:34 +0100

    meu primeiro projecto django

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from .models import Post
+
+def listar_posts(request):
+    posts = Post.objects.all().order_by('-data_de_criacao')
+    return render(request, 'blog/listar_posts.html', {'posts': posts})
+

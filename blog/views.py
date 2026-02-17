f1415532ecf8f0f751954b2c2eeaea2cb7cd2f65
Márcio Mendes <marciodev8@gmail.com>
2026-02-17 18:16:04 +0100

    Atualizações no site

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,3 @@ from .models import Post
 def listar_posts(request):
     posts = Post.objects.all().order_by('-data_de_criacao')
     return render(request, 'blog/listar_posts.html', {'posts': posts})
-
